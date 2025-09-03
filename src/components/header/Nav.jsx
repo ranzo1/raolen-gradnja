96f0ff47d43c4cb753bc7b989a4acbf1f02f9ffc
Macbook <macbook@192.168.1.11>
2025-09-03 23:30:47 +0200

    return link to old behavior

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { Link as ScrollLink } from "react-scroll";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
 import HoverLine from "@/src/components/animations/HoverLine";
@@ -8,41 +8,39 @@ const Nav = ({ containerStyles, linkStyles, links: customLinks }) => {
   const t = useTranslations("Header");
 
   const defaultLinks = [
-    { path: "home", name: t("home"), offset: 0 },
-    { path: "about", name: t("about"), offset: 40 },
-    { path: "plan", name: t("plan"), offset: -10 },
-    { path: "location", name: t("location"), offset: -760 },
-    { path: "facilities", name: t("facilities"), offset: -310 },
-    { path: "apartments", name: t("apartments"), offset: -10 },
-    { path: "projects", name: t("projects"), offset: -10 },
-    { path: "contact", name: t("contact"), offset: 0 },
+    { path: "home", name: t("home"), offset: 0, type: "scroll" },
+    { path: "about", name: t("about"), offset: 40, type: "scroll" },
+    { path: "plan", name: t("plan"), offset: -10, type: "scroll" },
+    { path: "location", name: t("location"), offset: -760, type: "scroll" },
+    { path: "facilities", name: t("facilities"), offset: -310, type: "scroll" },
+    { path: "apartments", name: t("apartments"), offset: -10, type: "scroll" },
+    { path: "projects", name: t("projects"), offset: -10, type: "scroll" },
+    { path: "contact", name: t("contact"), offset: 0, type: "scroll" },
   ];
 
   const links =
     customLinks && customLinks.length > 0 ? customLinks : defaultLinks;
 
-  // Smooth scroll function
-  const handleSmoothScroll = (e, path, offset) => {
-    const target = document.getElementById(path);
-    if (target) {
-      e.preventDefault();
-      const top =
-        target.getBoundingClientRect().top + window.pageYOffset + offset;
-      window.scrollTo({ top, behavior: "smooth" });
-    }
-  };
-
   return (
     <nav className={`${containerStyles}`}>
       {links.map((link, index) => (
         <div className="group" key={index}>
-          <a
-            href={`#${link.path}`} // crawlable
-            onClick={(e) => handleSmoothScroll(e, link.path, link.offset)}
-            className={`${linkStyles}`}
-          >
-            {link.name}
-          </a>
+          {link.type === "scroll" ? (
+            <ScrollLink
+              to={link.path}
+              spy={true}
+              smooth={true}
+              offset={link.offset}
+              duration={500}
+              className={`${linkStyles}`}
+            >
+              {link.name}
+            </ScrollLink>
+          ) : (
+            <Link href={link.path} className={`${linkStyles}`}>
+              {link.name}
+            </Link>
+          )}
           <HoverLine />
         </div>
       ))}
